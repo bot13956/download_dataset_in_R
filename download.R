5ed7109fc186adaf17a05ed7599c5678fb0762cd
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-07-03 17:24:26 -0500

    Add files via upload

---
+++ download.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\btayo\\Desktop\\grade_classifier")
+library(readr)
+#using download.file() function
+download.file("https://raw.githubusercontent.com/bot13956/datasets/master/introduction_to_physics_grades.csv", "grades.csv")
+df<-read.csv("grades.csv")
+head(df)
+#using read.csv() function
+df<-read.csv("https://raw.githubusercontent.com/bot13956/datasets/master/introduction_to_physics_grades.csv")
+head(df)
