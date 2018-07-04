1deca5b258fe156f34ee25389e02c043ca8e13e6
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-07-04 10:46:13 -0500

    Update download.R

--- download.R
+++ download.R
@@ -1,3 +1,5 @@
+#code to download a file from the internet using download.file() and read.csv() functions.
+#start by choosing the directory where the file should be downloaded
 setwd("C:\\Users\\btayo\\Desktop\\grade_classifier")
 library(readr)
 #using download.file() function
